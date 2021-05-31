7d3cdaed0c4027e586266be4e0cbb602cb512558
ligang <ligangag@yonyou.com>
2021-05-31 16:51:49 +0800

    spring-自定义注解reflection

---
+++ design-demo/src/test/java/net/gaven/designdemo/DesignDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.gaven.designdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
