2567e1a13ae418fbaf3f1af7eaffd218fe291764
ligang <ligangag@yonyou.com>
2021-06-02 16:01:33 +0800

    springdatajpa

---
+++ springdata-jpa/src/main/java/net/gaven/springdatajpa/SpringdataJpaApplication.java
@@ -0,0 +1,13 @@
+package net.gaven.springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdataJpaApplication.class, args);
+    }
+
+}
